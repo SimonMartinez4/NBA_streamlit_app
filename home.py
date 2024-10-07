6b2b4b2fa1a347e0fdbe6d1ac552bf4c86c618c0
Simon Martinez <simonmartinez4@gmail.com>
2024-10-07 17:17:50 +0200

    Homepage writing update

--- home.py
+++ home.py
@@ -21,14 +21,6 @@ def run() :
     Hello, my name is **Simon Martinez**. I am a data analyst with a lifelong passion for basketball, having been a player and an avid fan since the age of six.
     Combining my professional expertise in data analysis with my love for the game, I created this streamlit app to share my unique perspective on basketball.
     """)
-
-    st.image("img/michael-jordan---nba.jpg", use_column_width=True)
-
-    st.write("""
-    What interests me is identifying the elements that had the most influence during a game.
-    Utilizing Python libraries and the NBA API, we aim to answer intriguing questions that arise during the excitement of a match, representing and synthesizing facts that are too numerous to narrate.
-    """)
-
     # What You Will Find Here section
     st.header("What You Will Find Here")
 
@@ -38,10 +30,6 @@ def run() :
 
     st.write("""
     - **Match Dynamics**: Specific game reports that trace the ebb and flow of each match, capturing pivotal moments and shifts in momentum.
-    """)
-
-    st.write("""
-    - **In-depth Studies**: Comprehensive analyses on long-term trends and specific questions that have left a mark on the league. These studies provide deeper insights into historical and contemporary issues within the NBA.
     """)
 
     # Get in Touch section
